99edb225ba3ac28983ad021c5b97ec878a9fccd9
Rachel Metheny <rachelmetheny@Rachel-Methenys-MacBook-Pro.local>
2015-04-29 17:15:39 -0400

    i "message"

--- script.js
+++ script.js
@@ -4,13 +4,29 @@ var template = function(text) {
 
 var main = function() {
   $('form').submit(function() {
-      
-    
-    
+ 
+      var text = $('#todo').val(), 
+      html = template(text);
+   // console.log.(text);
+   
+   $('.list').append(html);
+
+
     return false;  
-  }
-  
-  
+ 
+  });
+
+
+  $('.glyphicon-star').click(function(){
+  	$(this).toggleClass("active");
+  });
+
+  $('.glyphicon-remove').click(function(){
+  	$(this).closest("p").remove();
+
+  });
+
+
 };
 
 $(document).ready(main);
